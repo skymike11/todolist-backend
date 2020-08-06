b6e5845c6900f7e3009e0863572a0255bcd01b8c
OOCLDM\lisa8 <lisa8@oocl.com>
2020-08-06 19:54:59 +0800

    [todolist-backend][domingo && sam] test : should_return_0_when_get_all_todo_given_0_todo

---
+++ src/test/java/com/example/todolist/ServiceTest/todoServiceTest.java
@@ -0,0 +1,37 @@
+package com.example.todolist.ServiceTest;
+
+import com.example.repository.TodoRepository;
+import com.example.todolist.dto.TodoRequestDto;
+import com.example.todolist.service.impl.TodoServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+public class todoServiceTest {
+    @InjectMocks
+    TodoServiceImpl todoService;
+
+    @Mock
+    TodoRepository todoRepository;
+
+    @Test
+    void should_return_0_when_get_all_todo_given_0_todo() {
+        //given
+
+        //when
+        when(todoRepository.findAll()).thenReturn(new ArrayList<>());
+        //then
+        assertEquals(0,todoService.getAllTodo().size());
+    }
+}
